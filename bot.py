9f5562d5244bf82cc25d591f8cd129249d120067
demosthenix <demosthenical@gmail.com>
2020-10-16 13:25:53 +0530

    first commit

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
 #driver = webdriver.Chrome('/Users/Demosthenix/Downloads/chromedriver-1')
